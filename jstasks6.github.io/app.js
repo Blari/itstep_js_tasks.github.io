5c2208da4327f85977951b1988981d0fdcf326ed
Blari <Blarior@gmail.com>
2019-11-03 16:50:27 +0300

    task 6

--- jstasks6.github.io/app.js
+++ jstasks6.github.io/app.js
@@ -1,193 +1,77 @@
 // Удалить в массиве все числа, которые повторяются
 //                         более двух раз.
-function genericRandomArr() {
-    let arr = [];
-    let r = Math.floor(Math.random() * 10);
-
-    for (let i = 0; i <= r; i++) {
-        arr[i] = Math.floor(Math.random() * 10);
-    }
-    return arr;
+function randomInteger(min, max) {
+    // получить случайное число от (min-0.5) до (max+0.5)
+    let rand = min - 0.5 + Math.random() * (max - min + 1);
+    return Math.round(rand);
 }
+function arrayPos(n) {
+    //var n = randomInteger(3, 5);
+    var array = [];
+    let array2 = [];
+    var str = "";
+    var str2 = "";
+    let sum = 0;
+    let sum2 = 0;
+    let strSum2 = "";
+    let sum3 = 0;
+    let strSum3 = "";
 
-function deleteEllems(arr) {
-    let newArray = [];
-    let newArray1 = [];
-    let newArray2 = [];
-    let newArray3 = [];
-    str = "";
-
-    for (let i = 0; i < arr.length; i++) {
-        newArray.push(arr[i]);
-    }
-
-    for (let i = 0; i < arr.length; i++) {
-        newArray1.push(arr[i]);
-    }
-
-    for (; arr.length != 0; ) {
-        var elem = arr.splice(0, 1);
-        elem[1] = 1;
-        for (var j = 0; j < arr.length; j++) {
-            if (elem[0] === arr[j]) {
-                elem[1]++;
-                arr.splice(j, 1);
-                j--;
-            }
+    for (var i = 0; i < n; i++) {
+        array[i] = [];
+        array2[i] = [];
+        for (var j = 0; j < n; j++) {
+            var elem = randomInteger(1, 9);
+            array[i].push(elem);
+            array2[i].push(elem);
+            //sum += elem;
         }
-
-        newArray2.push(elem);
+        str += array[i].join(" ") + " \n";
     }
 
-    for (let i = 0; i < newArray2.length; i++) {
-        if (newArray2[i][1] > 2) {
-            for (let j = 0; j < newArray1.length; j++) {
-                if (newArray2[i][0] === newArray1[j]) {
-                    newArray1.splice(j, 1);
-                    j--;
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j <= n - 2; j++) {
+            for (let k = 0; k <= n - 2; k++) {
+                if (array[i][k] > array[i][k + 1]) {
+                    let temp = array[i][k];
+                    array[i][k] = array[i][k + 1];
+                    array[i][k + 1] = temp;
                 }
             }
         }
+        str2 += array[i].join(" ") + " \n";
     }
 
-    return `Исходный массив: ${newArray}\nКонечный массив: ${newArray1}`;
-}
-
-//Удалить повторяющиеся элементы из массива
-
-function deleteSameElements(arr) {
-    let Arr1Copy = JSON.parse(JSON.stringify(arr));
-    let result = [];
-
-    for (let str of arr) {
-        if (!result.includes(str)) {
-            result.push(str);
-        }
-    }
-
-    return `Исходный массив: ${arr}\nИтоговый массив: ${result}`;
-}
-
-//Дан массив. Определить максимальную серию подряд элементов по возрастанию. Вывести ее
-function maxSerie(arr) {
-    let lens = [];
-    lens[1] = 0;
-    let max = 0;
-    let arr2 = [];
-    let str = "";
-
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] > arr[i - 1]) {
-            lens[i] = lens[i - 1] + 1;
-        } else {
-            lens[i] = 0;
-        }
-        max = Math.max(lens[i], max);
-    }
-
-    for (let i = 0; i < arr.length; i++) {
-        if (lens[i] === max) {
-            for (let j = i - lens[i]; j <= i; j++) {
-                str += String(arr[j]) + " ";
-            }
-            str += "\n";
-        }
-    }
-    return `Оригинальный массив: ${arr}\nМаксимальная последоательность = ${max +
-        1}\nВот она/они:\n${str}`;
-}
-
-//Найти два максимальных элемента массива
-function findTwoMaxNumber(arr) {
-    let max = arr[0];
-    let copyArr = JSON.parse(JSON.stringify(arr));
-    let max2 = arr[0];
-
-    if (arr.length == 1) {
-        return `Исходный массив: ${arr}\nМаксимум один: ${arr[0]}`;
-    } else {
-        for (let i = 0; i < arr.length; i++) {
-            if (arr[i] > max) max = arr[i];
-        }
-        for (let i = 0; i < copyArr.length; i++) {
-            if (copyArr[i] === max) {
-                copyArr.splice(i, 1);
-                i--;
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (i === j) {
+                sum += array2[i][j];
             }
         }
-        max2 = copyArr[0];
-
-        for (let i = 0; i < copyArr.length; i++) {
-            if (copyArr[i] > max2) max2 = copyArr[i];
-        }
-
-        return `Исходный массив: ${arr}\nПервый максимум: ${max}\nВторой максимум: ${max2}`;
-    }
-}
-
-function unZip(arr) {
-    // console.log(typeof arr);
-    // let newArray = arr.split(",");
-    // console.log(typeof newAarry);
-    let str = "";
-    if (arr[0] != 0) {
-        for (let i = 0; i < arr.length; i++) {
-            if (i % 2 != 0) {
-                for (let j = 0; j < arr[i]; j++) {
-                    str += 1;
-                }
-            } else {
-                for (let j = 0; j < arr[i]; j++) {
-                    str += 0;
-                }
-            }
-
-            str += " ";
-        }
-    } else if (arr[0] === 0) {
-        for (let i = 0; i < arr.length; i++) {
-            if (i % 2 != 0) {
-                for (let j = 0; j < arr[i]; j++) {
-                    str += 1;
-                }
-            } else {
-                for (let j = 0; j < arr[i]; j++) {
-                    str += 0;
-                }
-            }
-
-            str += " ";
-        }
     }
 
-    return `Шифр: ${arr}\nРасшифровка: ${str}`;
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j <= i; j++) {
+            sum2 += array2[i][j];
+            strSum2 += +array2[i][j];
+            if (array2[i][j + 1] != undefined) {
+                strSum2 += " + ";
+            }
+        }
+    }
+    for (let i = 0; i < n; i++) {
+        for (let j = i; j < n; j++) {
+            sum3 += array2[i][j];
+            strSum3 += +array2[i][j];
+            if (i < n - 1) {
+                strSum3 += " + ";
+            }
+        }
+    }
+    return `Исходный массив:\n${str}\nСтроки по возростанию:\n${str2}\nСумма элементов по диагонали (исходный массив): ${sum}\nСумма элементов ниже главной диагонали(включая главную диагональ): ${strSum2} = ${sum2}\nСумма элементов выше главной диагонали(включая главную диагональ): ${strSum3} = ${sum3}`;
 }
 
 document.getElementById("buttonT1").onclick = function() {
-    let r = genericRandomArr();
-    document.getElementById("textAreaT1").value = deleteEllems(r);
-};
-
-document.getElementById("buttonT2").onclick = function() {
-    let r = genericRandomArr();
-    document.getElementById("textAreaT2").value = deleteSameElements(r);
-};
-
-document.getElementById("buttonT3").onclick = function() {
-    let r = genericRandomArr();
-    document.getElementById("textAreaT3").value = maxSerie(r);
-};
-
-document.getElementById("buttonT4").onclick = function() {
-    let r = genericRandomArr();
-    document.getElementById("textAreaT4").value = findTwoMaxNumber(r);
-};
-
-document.getElementById("buttonT5").onclick = function() {
-    let r = genericRandomArr();
-    document.getElementById("textAreaT5").value = unZip(r);
-};
-document.getElementById("buttonT5-1").onclick = function() {
-    let r = document.getElementById("inputT5-1").value;
-    document.getElementById("textAreaT5").value = unZip(r);
+    n = document.getElementById("arrayNum").value;
+    document.getElementById("textAreaT1").value = arrayPos(n);
 };
