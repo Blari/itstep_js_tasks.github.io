e54f7935c370b59a2a305bd0b9687d312c2c6cb5
Blari <Blarior@gmail.com>
2019-11-15 23:03:34 +0300

    add normal modal window

--- jstasks9.github.io/app.js
+++ jstasks9.github.io/app.js
@@ -2,3 +2,8 @@ btn.addEventListener("click", () => {
   box.style.opacity = "1";
   box.style.visibility = "visible";
 });
+
+btn2.addEventListener("click", () => {
+  box.style.opacity = "0";
+  box.style.visibility = "hidden";
+});
