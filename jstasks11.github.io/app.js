0f5a3208ac2760bebcc940507fc9b8173b30e16d
Blari <Blarior@gmail.com>
2019-11-21 23:07:42 +0300

    first half of tsk 11 ready

--- jstasks11.github.io/app.js
+++ jstasks11.github.io/app.js
@@ -8,7 +8,13 @@ for (let i = 0; i < td.length; i++) {
 function tdFoo() {
   if (this.classList != "active") {
     this.classList.add("active");
+    let el = +document.getElementsByTagName("p")[0].innerHTML;
+    el += +this.innerHTML;
+    document.getElementsByTagName("p")[0].innerHTML = el;
   } else {
     this.classList.remove("active");
+    let el = +document.getElementsByTagName("p")[0].innerHTML;
+    el -= +this.innerHTML;
+    document.getElementsByTagName("p")[0].innerHTML = el;
   }
 }
