2e02f5e3ca3ad6604f07cc5d033cbd3eb0c81166
Blari <Blarior@gmail.com>
2019-11-22 23:32:28 +0300

    main function

--- jstasks11.github.io/app2.js
+++ jstasks11.github.io/app2.js
@@ -2,6 +2,8 @@ let img = document.getElementsByClassName("main-galery")[0].children[0]
   .children;
 let bigImg = document.getElementsByClassName("img-big")[0];
 
+let modal = document.getElementsByClassName("modal-row")[0];
+
 bigImg.addEventListener("click", closeBigImg);
 
 for (let i = 0; i < img.length; i++) {
@@ -10,8 +12,15 @@ for (let i = 0; i < img.length; i++) {
 
 function closeBigImg() {
   bigImg.src = "#";
+  modal.style.visibility = "hidden";
+  modal.style.opacity = "0";
+  bigImg.style.opacity = "0";
 }
 
 function clickFoo() {
   bigImg.src = this.src;
+  bigImg.style.opacity = "1";
+  bigImg.style.visibility = "visible";
+  modal.style.visibility = "visible";
+  modal.style.opacity = "1";
 }
