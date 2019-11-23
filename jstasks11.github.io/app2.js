495bf3ad72ac5b422ac017b01b06a06dce5f44f3
Blari <Blarior@gmail.com>
2019-11-24 01:31:16 +0300

    some fix

--- jstasks11.github.io/app2.js
+++ jstasks11.github.io/app2.js
@@ -13,7 +13,7 @@ for (let i = 0; i < img.length; i++) {
 }
 
 function closeBigImg() {
-  bigImg.style.backgroundImage = ``;
+  bigImg.style.backgroundImage = `none`;
   bigImg.style.visibility = "hidden";
   modal.style.visibility = "hidden";
   modal.style.opacity = "0";
@@ -22,7 +22,7 @@ function closeBigImg() {
 
 function clickFoo() {
   bigImg.style.backgroundImage = `url(${this.src})`;
-  bigImg.style.backgroundSize = `cover`;
+  bigImg.style.backgroundSize = `contain`;
   bigImg.style.opacity = "1";
   bigImg.style.visibility = "visible";
   info.innerHTML = this.getAttribute("data-info");
