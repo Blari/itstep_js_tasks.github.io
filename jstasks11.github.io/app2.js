011c4b64df3d2ac5083c92cfe7dbe30c3a6711b7
Blari <Blarior@gmail.com>
2019-11-24 01:17:57 +0300

    Change img type to <i>. Add text from data atribute

--- jstasks11.github.io/app2.js
+++ jstasks11.github.io/app2.js
@@ -2,8 +2,10 @@ let img = document.getElementsByClassName("main-galery")[0].children[0]
   .children;
 let bigImg = document.getElementsByClassName("img-big")[0];
 
-let modal = document.getElementsByClassName("modal-row")[0];
+let info = bigImg.children[0].children[0];
 
+let modal = document.getElementsByClassName("modal-row")[0];
+console.log(info);
 bigImg.addEventListener("click", closeBigImg);
 
 for (let i = 0; i < img.length; i++) {
@@ -11,16 +13,20 @@ for (let i = 0; i < img.length; i++) {
 }
 
 function closeBigImg() {
-  bigImg.src = "#";
+  bigImg.style.backgroundImage = ``;
+  bigImg.style.visibility = "hidden";
   modal.style.visibility = "hidden";
   modal.style.opacity = "0";
   bigImg.style.opacity = "0";
 }
 
 function clickFoo() {
-  bigImg.src = this.src;
+  bigImg.style.backgroundImage = `url(${this.src})`;
+  bigImg.style.backgroundSize = `cover`;
   bigImg.style.opacity = "1";
   bigImg.style.visibility = "visible";
+  info.innerHTML = this.getAttribute("data-info");
+
   modal.style.visibility = "visible";
   modal.style.opacity = "1";
 }
