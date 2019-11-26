1b980d683cfe5a4d1d96e4500292ba30cddec16c
Blari <Blarior@gmail.com>
2019-11-26 23:07:42 +0300

    row calc

--- jstasks12.github.io/app.js
+++ jstasks12.github.io/app.js
@@ -22,11 +22,28 @@ for (let i = 0; i < table.children.length; i++) {
   for (let j = 0; j < table.children.length; j++) {
     tdSum += +table.children[i].children[j].innerHTML;
   }
-  console.log(`Сумма чисел: ${tdSum}`);
   td.innerHTML = tdSum;
+  td.classList.add("summ");
   table.children[i].append(td);
   tdSum = 0;
 }
+
+//Подсчитываем сумму в столбцах
+let tdSumCol = 0;
+let tr = document.createElement("tr");
+
+for (let i = 0; i < table.children.length; i++) {
+  let td = document.createElement("td");
+  for (let j = 0; j < table.children.length; j++) {
+    tdSumCol += +table.children[j].children[i].innerHTML;
+  }
+  td.innerHTML = tdSumCol;
+  td.classList.add("summ");
+  tr.append(td);
+  tdSumCol = 0;
+}
+tr.insertAdjacentHTML("beforeend", `<td class="summ">-</td>`);
+table.append(tr);
 
 //Находим кнопку добавления колонки
 const addCol = document.getElementsByClassName("add-col")[0];
@@ -37,11 +54,33 @@ const addRow = document.getElementsByClassName("add-row")[0];
 addRow.addEventListener("click", addRowFun);
 
 //Добавление колонок
+
 function addColFun() {
+  //Удаляем столбец с суммой
+  let table = document.getElementsByTagName("table")[0];
+  for (let i = 0; i < table.children.length; i++) {
+    table.children[i].lastChild.remove();
+  }
+
+  //Добавляем новый столбец
   for (let i = 0; i < table.children.length; i++) {
     let td = document.createElement("td");
     td.innerHTML = Math.floor(Math.random() * 10);
     table.children[i].append(td);
+  }
+
+  //Подсчитываем сумму и добавляем в конец
+  let tdSum = 0;
+  for (let i = 0; i < table.children.length; i++) {
+    let td = document.createElement("td");
+
+    for (let j = 0; j < table.children[i].children.length; j++) {
+      tdSum += +table.children[i].children[j].innerHTML;
+    }
+    td.innerHTML = tdSum;
+    td.classList.add("summ");
+    table.children[i].append(td);
+    tdSum = 0;
   }
 }
 
