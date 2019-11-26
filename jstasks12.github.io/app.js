63f1ef99fc8e7f85fced05c1e7b431311b16bf3c
Blari <Blarior@gmail.com>
2019-11-26 18:27:02 +0300

    base table and links

--- jstasks12.github.io/app.js
+++ jstasks12.github.io/app.js
@@ -1,20 +1,16 @@
-let td = document.getElementsByTagName("td");
-
-for (let i = 0; i < td.length; i++) {
-  td[i].innerHTML = Math.floor(Math.random() * 100);
-  td[i].addEventListener("click", tdFoo);
-}
-
-function tdFoo() {
-  if (this.classList != "active") {
-    this.classList.add("active");
-    let el = +document.getElementsByTagName("p")[0].innerHTML;
-    el += +this.innerHTML;
-    document.getElementsByTagName("p")[0].innerHTML = el;
-  } else {
-    this.classList.remove("active");
-    let el = +document.getElementsByTagName("p")[0].innerHTML;
-    el -= +this.innerHTML;
-    document.getElementsByTagName("p")[0].innerHTML = el;
+const box = document.getElementsByClassName("box")[0];
+const table = document.createElement("table");
+table.classList.add("table");
+table.classList.add("table-bordered");
+//Создание таблицы
+for (let i = 0; i < 3; i++) {
+  const tr = document.createElement("tr");
+  table.append(tr);
+  for (let j = 0; j < 3; j++) {
+    const td = document.createElement("td");
+    td.innerHTML = Math.floor(Math.random() * 10);
+    tr.append(td);
   }
 }
+
+box.append(table);
