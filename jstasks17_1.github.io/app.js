938352d3c497d500bb8fba7e6ff623d21d68c3f8
Blari <Blarior@gmail.com>
2019-12-15 14:53:54 +0300

    min sec mls

--- jstasks17_1.github.io/app.js
+++ jstasks17_1.github.io/app.js
@@ -3,13 +3,60 @@
 //     task1Fun();
 //   }
 // }
+
 let seconds = 0;
 let minutes = 0;
 let hours = 0;
+let ms = 0;
+
 let start = document.querySelector(".start");
-start.addEventListener("click", task1Fun);
-function task1Fun() {
-  let time = new Date().getTime();
+let stop = document.querySelector(".stop");
+start.addEventListener("click", timer);
+stop.addEventListener("click", stopFu);
+
+let tic = setInterval(render, 10);
+
+//Запуск таймера
+function timer() {
+  tic;
+}
+//Отрисовка в дом
+function render() {
   let buttons = document.querySelectorAll(".disable");
-  console.log(time);
+  ms++;
+  if (ms < 100) {
+    ms < 10
+      ? (buttons[3].innerHTML = "00" + ms)
+      : ms < 100
+      ? (buttons[3].innerHTML = ms + "0")
+      : ms;
+  } else {
+    ms = 0;
+    seconds++;
+    if (seconds < 60) {
+      if (seconds < 10) {
+        buttons[2].innerHTML = "0" + seconds;
+        buttons[3].innerHTML = ms;
+      } else {
+        buttons[2].innerHTML = seconds;
+        buttons[3].innerHTML = ms;
+      }
+    } else {
+      seconds = 0;
+      minutes++;
+      if (minutes < 10) {
+        buttons[1].innerHTML = "0" + minutes;
+        buttons[2].innerHTML = seconds;
+        buttons[3].innerHTML = ms;
+      } else {
+        buttons[1].innerHTML = minutes;
+        buttons[2].innerHTML = seconds;
+        buttons[3].innerHTML = ms;
+      }
+    }
+  }
+}
+//Остановка секундомра
+function stopFu() {
+  clearInterval(tic);
 }
