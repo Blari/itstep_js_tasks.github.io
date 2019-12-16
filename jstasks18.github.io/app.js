d8ff745c6bd430b3e7222292d4b9a5caab9e5dc4
Blari <Blarior@gmail.com>
2019-12-16 23:43:52 +0300

    main part of task 18

---
+++ jstasks18.github.io/app.js
@@ -0,0 +1,28 @@
+let btn = document.querySelector(".btn");
+btn.addEventListener("click", cookSet);
+let arr = ["Доброго утра", "Доброго дня", "Доброго вечера", "Доброй ночи"];
+//Приветствие в зависимости от времени
+function timeDay() {
+  let p = document.querySelector(".greetings");
+  let date = new Date();
+  let day = "";
+  date.getHours() >= 0 && date.getHours() < 6
+    ? (day = arr[3])
+    : date.getHours() >= 6 && date.getHours() < 12
+    ? (day = arr[0])
+    : date.getHours() >= 12 && date.getHours() < 18
+    ? (day = arr[1])
+    : date.getHours() >= 18 && date.getHours() < 24
+    ? (day = arr[2])
+    : (day = "");
+  p.innerHTML = `${day}, пользователь! Представся и мы продолжим.`;
+}
+function cookSet() {
+  let input = document.querySelectorAll("input");
+  console.log(input[0].value);
+  console.log(input[1].value);
+  Cookies.set("name", input[0].value);
+  Cookies.set("sername", input[1].value);
+}
+
+timeDay();
