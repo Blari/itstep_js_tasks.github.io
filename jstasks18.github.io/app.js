4480b8a076e2c6f3b83e37b755b85088ea3b549a
Blari <Blarior@gmail.com>
2019-12-17 10:10:21 +0300

    add jumbatron

--- jstasks18.github.io/app.js
+++ jstasks18.github.io/app.js
@@ -30,4 +30,52 @@ function cookGet() {
   console.log(Cookies.get());
 }
 
+function jumbotron(
+  name = "Имя",
+  surname = "Фамилия",
+  date = "17.10.21019",
+  count = 0
+) {
+  let app = document.querySelector(".app");
+  let div = document.createElement("div");
+  let h1 = document.createElement("h1");
+  let p = document.createElement("p");
+  let hr = document.createElement("hr");
+  let p2 = document.createElement("p");
+  let p3 = document.createElement("p");
+  let a = document.createElement("a");
+
+  div.classList.add("jumbotron");
+  h1.classList.add("display-4");
+  h1.append(`Привет, ${name} ${surname}`);
+  p.classList.add("lead");
+  hr.classList.add("my-4");
+  p.append(`Последний раз ты сюда заходил ${date}.`);
+  p.append(`И это твой ${count} заход.`);
+  p2.classList.add("lead");
+  p2.append(
+    "Использются служебные классы для типографики и расстояния содержимого в контейнере большего размера."
+  );
+  a.classList.add("btn");
+  a.classList.add("btn-primary");
+  a.classList.add("btn-lg");
+  a.href = "#";
+  a.setAttribute("role", "button");
+  a.addEventListener("click", cookClean);
+  a.append("Выход");
+
+  p3.append(a);
+  div.append(h1);
+  div.append(p);
+  div.append(hr);
+  div.append(p2);
+  div.append(p3);
+  app.append(div);
+}
+
+function cookClean() {
+  Cookies.remove("name");
+}
+
 timeDay();
+jumbotron();
