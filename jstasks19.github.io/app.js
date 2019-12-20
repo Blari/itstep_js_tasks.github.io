3ea4dabb501be9e4293c3f80da3d7028c8731ed2
Blari <Blarior@gmail.com>
2019-12-20 11:52:02 +0300

    more code

--- jstasks19.github.io/app.js
+++ jstasks19.github.io/app.js
@@ -1,1 +1,14 @@
+let cont = document.querySelectorAll(".container-fluid")[0];
+cont.onwheel = functionRef;
 
+function functionRef(e) {
+  let row = document.querySelectorAll(".row");
+
+  if (event.deltaY < 0) {
+    row[0].style.display = "none";
+    row[1].style.display = "flex";
+  } else {
+    row[1].style.display = "none";
+    row[0].style.display = "flex";
+  }
+}
