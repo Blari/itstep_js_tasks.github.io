51a697a3cc8b73f04a03e9849329382904fbf7d2
Blari <Blarior@gmail.com>
2019-12-20 11:56:13 +0300

    background

--- jstasks19.github.io/app.js
+++ jstasks19.github.io/app.js
@@ -7,8 +7,11 @@ function functionRef(e) {
   if (event.deltaY < 0) {
     row[0].style.display = "none";
     row[1].style.display = "flex";
+    cont.style.background = "#F9F7ED";
   } else {
     row[1].style.display = "none";
     row[0].style.display = "flex";
+
+    cont.style.background = "#cdeb8b";
   }
 }
