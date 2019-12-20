77c996107007cc8375e944f4eeb9ba498450cce5
Blari <Blarior@gmail.com>
2019-12-20 12:18:39 +0300

    some fix

--- jstasks19.github.io/app.js
+++ jstasks19.github.io/app.js
@@ -1,9 +1,19 @@
 let cont = document.querySelectorAll(".container-fluid")[0];
 cont.onwheel = functionRef;
 
+function findFlex() {
+  let row = document.getElementsByClassName("row");
+  let n = 0;
+  for (let i = 0; i < row.length; i++) {
+    row[i].style.display == "flex" ? (n = i) : null;
+  }
+  return n;
+}
+
 function functionRef(e) {
   let row = document.querySelectorAll(".row");
-
+  let n = findFlex();
+  console.log(n);
   if (event.deltaY < 0) {
     row[0].style.display = "none";
     row[1].style.display = "flex";
