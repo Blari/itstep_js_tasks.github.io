3f904c03afc55c8cc900bb7dfc52ecf0af925940
Blari <Blarior@gmail.com>
2019-12-21 13:06:41 +0300

    main part of whell ready

--- jstasks19.github.io/app.js
+++ jstasks19.github.io/app.js
@@ -1,14 +1,13 @@
+let row = document.getElementsByClassName("row");
 let cont = document.querySelectorAll(".container-fluid")[0];
 cont.onwheel = functionRef;
-
-function hideRow() {
-  let row = document.querySelectorAll(".row");
-  console.log(row);
-  for (let i = 1; i < row.length; i++) {
+//Скрываем лишние блоки
+function hideRow(x) {
+  for (let i = x; i < row.length; i++) {
     row[i].style.display = "none";
   }
 }
-
+//Номер "включенного" блока
 function findFlex() {
   let row = document.getElementsByClassName("row");
   let n = 0;
@@ -19,17 +18,47 @@ function findFlex() {
 }
 
 function functionRef(e) {
-  let row = document.querySelectorAll(".row");
   let n = findFlex();
-  if (event.deltaY > 0) {
-    n == row.length - 1 ? (n = 0) : n - 1;
 
-    for (let i = 0; i < row.length; i++) {
-      row[i].style.display = "none";
-    }
+  if (event.deltaY > 0) {
+    n === 2 ? (n = -1) : n;
+
+    hideRow(0);
+
     row[n + 1].style.display = "flex";
-    cont.style.background = "#F9F7ED";
+    switch (n) {
+      case -1:
+        cont.style.background = "#C3D9FF";
+        break;
+
+      case 0:
+        cont.style.background = "#CDEB8B";
+        break;
+
+      case 1:
+        cont.style.background = "#FFFF88";
+        break;
+    }
+  } else {
+    n === 0 ? (n = 3) : n;
+
+    hideRow(0);
+
+    row[n - 1].style.display = "flex";
+    switch (n) {
+      case 1:
+        cont.style.background = "#C3D9FF";
+        break;
+
+      case 2:
+        cont.style.background = "#CDEB8B";
+        break;
+
+      case 3:
+        cont.style.background = "#FFFF88";
+        break;
+    }
   }
 }
 
-hideRow();
+hideRow(1);
