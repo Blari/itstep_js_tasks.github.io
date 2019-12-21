7a8070478f95a9734090579b2cdfe19cfa8eaca5
Blari <Blarior@gmail.com>
2019-12-21 17:58:00 +0300

    more errors

--- jstasks19.github.io/app.js
+++ jstasks19.github.io/app.js
@@ -12,19 +12,18 @@ function hideRow(x) {
 
 function cleanUp() {
   for (let i = 0; i < row.length; i++) {
-    if (mcol[i].classList.contains("fadeInUp"))
-      mcol[i].classList.remove("fadeInUp");
-    if (mcol[i].classList.contains("fadeInDown"))
-      mcol[i].classList.remove("fadeInDown");
-  }
-}
-
-function cleanDown() {
-  for (let i = 0; i < row.length; i++) {
     if (mcol[i].classList.contains("fadeOutDown"))
       mcol[i].classList.remove("fadeOutDown");
     if (mcol[i].classList.contains("fadeInDown"))
       mcol[i].classList.remove("fadeInDown");
+  }
+}
+function cleanDown() {
+  for (let i = 0; i < row.length; i++) {
+    if (mcol[i].classList.contains("fadeOutUp"))
+      mcol[i].classList.remove("fadeOutUp");
+    if (mcol[i].classList.contains("fadeInUp"))
+      mcol[i].classList.remove("fadeInUp");
   }
 }
 //Номер "включенного" блока
@@ -41,18 +40,17 @@ function functionRef(e) {
   let n = findFlex();
 
   if (event.deltaY > 0) {
-    console.log(event.deltaY);
     //Вниз
-    mcol[n].classList.add("fadeOutDown"); //Текущий элемент пропадает вниз
+    mcol[n].classList.add("fadeOutDown"); //Текущий элемент "пропадает вниз"
 
     n === 2 ? (n = -1) : n;
-    mcol[n + 1].classList.add("fadeInDown"); //Следующий элемент появляется сверху-вниз
+    mcol[n + 1].classList.add("fadeInDown"); //Следующий элемент появляется "сверху-вниз"
 
     setTimeout(() => {
-      hideRow(0);
-      // cleanDown();
+      hideRow(0); //Прячем все элементы
+      //Показываем "следующий" элемент
       row[n + 1].style.display = "flex";
-
+      //Меняем цвет фона
       switch (n) {
         case -1:
           cont.style.background = "#C3D9FF";
@@ -67,16 +65,17 @@ function functionRef(e) {
           break;
       }
     }, 250);
-    mcol[n + 1].classList.remove("fadeOutDown");
+    setTimeout(() => {
+      cleanUp();
+    }, 1200);
   } else {
-    console.log(event.deltaY);
     //Вверх
     mcol[n].classList.add("fadeOutUp");
     n === 0 ? (n = 3) : n;
     mcol[n - 1].classList.add("fadeInUp");
     setTimeout(() => {
       hideRow(0);
-      cleanDown();
+      // cleanDown();
       row[n - 1].style.display = "flex";
       switch (n) {
         case 1:
@@ -92,7 +91,9 @@ function functionRef(e) {
           break;
       }
     }, 250);
-    mcol[n - 1].classList.remove("fadeOutUp");
+    setTimeout(() => {
+      cleanDown();
+    }, 1200);
   }
 }
 
